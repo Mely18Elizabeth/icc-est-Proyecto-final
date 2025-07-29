1fadc4c4c93eb51401de48b0decbd5c31c6e7fe4
Dayanna Chacha <jchachav@est.ups.edu.ec>
2025-07-28 23:27:05 -0500

    commit 4 Dayanna: Se creo los resultados del algoritmo

--- src/main/java/models/AlgorithmResult.java
+++ src/main/java/models/AlgorithmResult.java
@@ -1,4 +1,40 @@
 package models;
 
-public class AlgorithmResult {
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public class AlgorithmResult implements Serializable {
+
+    private final String algorithmName;
+    private final int pathLength;
+    private final long executionTimeNanos;
+    private final LocalDateTime timestamp;
+
+    public AlgorithmResult(String algorithmName, int pathLength, long executionTimeNanos) {
+        this.algorithmName = algorithmName;
+        this.pathLength = pathLength;
+        this.executionTimeNanos = executionTimeNanos;
+        this.timestamp = LocalDateTime.now();
+    }
+
+    public String getAlgorithmName() {
+        return algorithmName;
+    }
+
+    public int getPathLength() {
+        return pathLength;
+    }
+
+    public long getExecutionTimeNanos() {
+        return executionTimeNanos;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    @Override
+    public String toString() {
+        return algorithmName + "," + pathLength + "," + executionTimeNanos + "," + timestamp;
+    }
 }
