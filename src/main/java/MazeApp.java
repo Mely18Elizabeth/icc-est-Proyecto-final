67448a56bc08cc2bd8d9409ba1e1e39a275fffe4
Dayanna Chacha <jchachav@est.ups.edu.ec>
2025-07-28 23:20:08 -0500

    commit 1 Dayanna: Se creó una aplicación gráfica en Java que permite resolver laberintos mediante distintos algoritmos (DFS, BFS, recursivos y backtracking), integrando una interfaz Swing y registrando los resultados mediante un controlador central.

--- src/main/java/MazeApp.java
+++ src/main/java/MazeApp.java
@@ -1,2 +1,33 @@
+import controllers.MazeController;
+import dao.AlgorithmResultDAO;
+import dao.daoImpl.AlgorithmResultDAOFile;
+import solver.solverImpl.*;
+
+import views.MazeFrame;
+import views.MazePanel;
+
+import javax.swing.*;
+
 public class MazeApp {
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            AlgorithmResultDAO resultDAO = new AlgorithmResultDAOFile();
+
+            MazePanel panel = new MazePanel();
+
+            MazeController controller = new MazeController(resultDAO);
+            controller.setMatriz(panel.getCells());
+            panel.setController(controller);
+
+            controller.registrarAlgoritmo("DFS", new MazeSolverDFS());
+            controller.registrarAlgoritmo("BFS", new MazeSolverBFS());
+            controller.registrarAlgoritmo("RECURSIVO", new MazeSolverRecursivo());
+            controller.registrarAlgoritmo("RECURSIVO COMPLETO", new MazeSolverRecursivoCompleto());
+            controller.registrarAlgoritmo("RECURSIVO COMPLETO BT", new MazeSolverRecursivoCompletoBT());
+            controller.registrarAlgoritmo("BACKTRACKING", new MazeSolverRecursivoCompletoBT());
+
+            new MazeFrame(controller, panel);
+        });
+    }
 }
