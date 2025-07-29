288cd0c54e7a75bb8dae5e2ed7b35b731f9263f6
Dayanna Chacha <jchachav@est.ups.edu.ec>
2025-07-28 23:28:59 -0500

    commit 6 Dayanna: Se creo las listas de cada path y los resultados de las Cell visitados

--- src/main/java/models/SolveResults.java
+++ src/main/java/models/SolveResults.java
@@ -1,4 +1,23 @@
 package models;
 
+import java.util.List;
+import java.util.Set;
+
 public class SolveResults {
+
+    private final List<Cell> path;
+    private final Set<Cell> visited;
+
+    public SolveResults(List<Cell> path, Set<Cell> visited) {
+        this.path = path;
+        this.visited = visited;
+    }
+
+    public List<Cell> getPath() {
+        return path;
+    }
+
+    public Set<Cell> getVisited() {
+        return visited;
+    }
 }
